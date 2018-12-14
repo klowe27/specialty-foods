08f549f693d7168d5629709882de0d774ed54b8b
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-14 14:14:09 -0800

    Add root path and display product scopes on homepage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,7 @@ class MainController < ApplicationController
   def index
     @usa_made_products = Product.usa_made
     @most_recent_products = Product.three_most_recent
+    @most_reviews = Product.most_reviews
 
     render :index
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,16 @@ class Product < ActiveRecord::Base
 
   scope :usa_made, -> { where(country_origin: "USA")}
 
-  scope :three_most_recent, -> { product(created_at: :desc).limit(3)}
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+
+  scope :most_reviews, -> {(
+    select("products.id, products.name, products.cost,
+    products.country_origin, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(8)
+    )}
 
   validates :name, :cost, :country_origin, :presence => true
 end
