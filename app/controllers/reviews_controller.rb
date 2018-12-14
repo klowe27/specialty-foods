985afbb10a0c5d324f60a5829eb1eeac2abda39d
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-14 12:09:53 -0800

    Update products form and routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,37 @@
-class ProductsController < ApplicationController
-  def index
-    @reviews = Product.all
-    render :index
+class ReviewsController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@review.product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:list_id])
+    @review = @list.reviews.find(params[:id])
+    @review.destroy
+    redirect_to list_path(@review.list)
   end
 end
