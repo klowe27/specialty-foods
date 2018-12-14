5b84251a04c2ecef41d4d3ed2bc751a5002e4546
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-14 11:04:11 -0800

    Database not seeding with validation; working on fix

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,10 +3,7 @@ class Review < ActiveRecord::Base
 
   validates :author, :presence => true
   validates :content_body, :presence => true
-  validates :content_body, length: { minimum: 50, maximum: 250, wrong_length: "Must be between 50 to 250 characters." }
-  validates :rating, :presence => true
-  validates :rating, numericality: { integer: true}
-  validates :rating, numericality: { greater_than: 0}
-  validates :rating, numericality: {less_than: 6}
+  validates :content_body, length: { within: 50..250 }
+  validates :rating, :presence => true, numericality: { integer: true, less_than: 6,  greater_than: 0 }
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,22 @@
 Product.destroy_all
 Review.destroy_all
 
-50.times do |index|
-  name =
-  Product.create(name: Faker::Food.fruits,
-    cost: (10 * (rand(200).to_f / 100).round(2)), country_origin: "USA")
+10.times do |index|
+  countries = ["USA", "China", "Canada", "France", "Mexico"]
+  countries.each do |country|
+    Product.create(name: Faker::Food.fruits,
+      cost: (10 * (rand(200).to_f / 100).round(2)), country_origin: country)
+  end
 end
 
 5.times do |index|
   products = Product.all
   products.each do |product|
     Review.create(author: Faker::Friends.character,
-      content_body: Faker::Lorem.characters(200),
+      content_body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam efficitur, erat non dictum aliquet, lacus diam hendrerit eros, at dictum enim tellus in elit. Cras a luctus ex, ultricies ullamcorper justo.",
       rating: (rand(5)+1), product_id: product.id)
   end
 end
 
 p "Created #{Product.count} products"
-p "Create #{Review.count} reviews"
+p "Created #{Review.count} reviews"
