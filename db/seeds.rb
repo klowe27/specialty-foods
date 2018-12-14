f444fb224b6ca091458e4c2f27cfecefc2590dc9
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-14 09:21:32 -0800

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Review.destroy_all
+
+50.times do |index|
+  name =
+  Product.create(name: Faker::Food.fruits,
+    cost: (10 * (rand(200).to_f / 100).round(2)), country_origin: "USA")
+end
+
+5.times do |index|
+  products = Product.all
+  products.each do |product|
+    Review.create(author: Faker::Friends.character,
+      content_body: Faker::Lorem.characters(200),
+      rating: (rand(5)+1), product_id: product.id)
+  end
+end
+
+p "Created #{Product.count} products"
+p "Create #{Review.count} reviews"
