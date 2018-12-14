aee342ae37f3fb9e429222913a87da165c08ee77
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-12-14 09:53:52 -0800

    Add additional validation specs; fails

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :product
-  
+
   validates :author, :content_body, :rating, :presence => true
+  validates_associated :product
+  validates :content_body, length: { minimum: 50, maximum: 250, wrong_length: "Must be between 50 to 250 characters." }
+  validates :rading, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 1 }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,6 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
+  it { should validate_numericality_of :rating}
+  it { should validate_length_of :body_content}
 end
